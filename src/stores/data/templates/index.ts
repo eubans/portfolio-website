12a05b50cfb4314df9ea417c5d11a76f80748e52
Mark Eubans <markivaneubans@gmail.com>
2024-01-12 17:43:05 +0800

    fix: stores data templates in gitignore

---
+++ src/stores/data/templates/index.ts
@@ -0,0 +1,9 @@
+/**
+ * NOTE: This file serve as your template.
+ * Please move this to src/stores/data.
+ */
+
+export * from './ExperiencesData'
+export * from './SocialsData'
+export * from './TestimonialsData'
+export * from './WorksData'
